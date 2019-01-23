3a8dde60ef996cbefcead71221d69a49b1d88409
hacktiger <hacktiegr1989@gmail.com>
2019-01-23 21:30:57 +0700

    implement load more on guide list fragment

--- app/src/main/java/com/sc2guide/sc2_guides_android/viewmodel/AllGuideViewModel.java
+++ app/src/main/java/com/sc2guide/sc2_guides_android/viewmodel/AllGuideViewModel.java
@@ -4,7 +4,6 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.support.annotation.NonNull;
-import android.util.Log;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,6 +11,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.sc2guide.sc2_guides_android.data.model.Guide;
 import com.sc2guide.sc2_guides_android.data.model.GuideBodyItem;
+import com.sc2guide.sc2_guides_android.error.NotPossibleException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -21,7 +21,7 @@ import java.util.Objects;
 public class AllGuideViewModel extends ViewModel {
     private MutableLiveData<List<Guide>> allGuides;
     private String reference = "guides";
-    private int guideNumLimit = 4;
+    private int guideNumLimit = 7;
     private String lastItemKey = "";
 
     public void setLastItemKey(String lastItemKey) {
@@ -58,37 +58,13 @@ public class AllGuideViewModel extends ViewModel {
                             List<Guide> tempListGuide = new ArrayList<>();
                             for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                                 try {
-                                    // get all the attributes
-                                    String id = snapshot.getKey();
-                                    String title = snapshot.child("title").getValue().toString();
-                                    String my_race = snapshot.child("myRace").getValue().toString();
-                                    String op_race = snapshot.child("opRace").getValue().toString();
-                                    String author_id = snapshot.child("authorId").getValue().toString();
-                                    String author_email = snapshot.child("authorName").getValue().toString();
-                                    String current_time = snapshot.child("date").getValue().toString();
-                                    // list to hold all the guide body items
-                                    List<GuideBodyItem> mList = new ArrayList<>();
-                                    // get guide body items
-                                    for (DataSnapshot mySnap : snapshot.child("guideBodyItems").getChildren()) {
-                                        GuideBodyItem item = new GuideBodyItem(mySnap.child("type").getValue().toString(), mySnap.child("body").getValue().toString());
-                                        mList.add(item);
-                                    }
-                                    // add the snapshot to the temp list
-                                    tempListGuide.add(new Guide(id, title, my_race, op_race, author_id, author_email, mList, current_time));
-                                } catch (Exception e) {
+                                    tempListGuide.add(retrieveDataToModel(snapshot));
+                                } catch (NotPossibleException e) {
                                     e.printStackTrace();
                                 }
                             }
                             // reverse the temp list
-                            Collections.reverse(tempListGuide);
-                            List<Guide> newList = new ArrayList<>();
-                            for (int i = 0; i < tempListGuide.size() - 1; i++) {
-                                newList.add(tempListGuide.get(i));
-                                Log.d("ZZKKK", tempListGuide.get(i).getTitle());
-                            }
-                            lastItemKey = tempListGuide.get(tempListGuide.size() - 1).getId();
-                            // set value to the mutable live data
-                            allGuides.setValue(newList);
+                            handleLoadGuideLogic(tempListGuide);
                         }
                     }
 
@@ -112,48 +88,15 @@ public class AllGuideViewModel extends ViewModel {
                         if (dataSnapshot.exists()) {
                             List<Guide> tempListGuide = new ArrayList<>();
                             for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                // add the snapshot to the temp list
                                 try {
-                                    // get all the attributes
-                                    String id = snapshot.getKey();
-                                    String title = snapshot.child("title").getValue().toString();
-                                    String my_race = snapshot.child("myRace").getValue().toString();
-                                    String op_race = snapshot.child("opRace").getValue().toString();
-                                    String author_id = snapshot.child("authorId").getValue().toString();
-                                    String author_email = snapshot.child("authorName").getValue().toString();
-                                    String current_time = snapshot.child("date").getValue().toString();
-                                    // list to hold all the guide body items
-                                    List<GuideBodyItem> mList = new ArrayList<>();
-                                    // get guide body items
-                                    for (DataSnapshot mySnap : snapshot.child("guideBodyItems").getChildren()) {
-                                        GuideBodyItem item = new GuideBodyItem(mySnap.child("type").getValue().toString(), mySnap.child("body").getValue().toString());
-                                        mList.add(item);
-                                    }
-                                    // add the snapshot to the temp list
-                                    tempListGuide.add(new Guide(id, title, my_race, op_race, author_id, author_email, mList, current_time));
-                                } catch (Exception e) {
+                                    tempListGuide.add(retrieveDataToModel(snapshot));
+                                } catch (NotPossibleException e) {
                                     e.printStackTrace();
                                 }
                             }
-                            // reverse the temp list
-                            Collections.reverse(tempListGuide);
                             // set value to the mutable live data
-                            if (lastItemKey.equals("END")) {
-                                return;
-                            }
-
-                            if (tempListGuide.size() < guideNumLimit) {
-                                Objects.requireNonNull(allGuides.getValue()).addAll(tempListGuide);
-                                lastItemKey = "END";
-                                return;
-                            }
-
-                            List<Guide> newList = new ArrayList<>();
-                            for (int i = 0; i < tempListGuide.size() - 1; i++) {
-                                newList.add(tempListGuide.get(i));
-                            }
-                            lastItemKey = tempListGuide.get(tempListGuide.size() - 1).getId();
-                            Objects.requireNonNull(allGuides.getValue()).addAll(newList);
-                            // TODO: test change to post value later
+                            handleLoadMoreLogic(tempListGuide);
                         }
                     }
                     @Override
@@ -161,5 +104,72 @@ public class AllGuideViewModel extends ViewModel {
                         databaseError.getCode();
                     }
                 });
+    }
+
+    /**
+     * @effects: use the data from the snapshot to make a new Guide();
+     * @param snapshot
+     * @return a new Guide Object
+     * @throws NotPossibleException
+     */
+    private Guide retrieveDataToModel(DataSnapshot snapshot) throws NotPossibleException {
+        // get all the attributes
+        String id = snapshot.getKey();
+        String title = snapshot.child("title").getValue().toString();
+        String my_race = snapshot.child("myRace").getValue().toString();
+        String op_race = snapshot.child("opRace").getValue().toString();
+        String author_id = snapshot.child("authorId").getValue().toString();
+        String author_email = snapshot.child("authorName").getValue().toString();
+        String current_time = snapshot.child("date").getValue().toString();
+        // list to hold all the guide body items
+        List<GuideBodyItem> mList = new ArrayList<>();
+        // get guide body items
+        for (DataSnapshot mySnap : snapshot.child("guideBodyItems").getChildren()) {
+            GuideBodyItem item = new GuideBodyItem(mySnap.child("type").getValue().toString(), mySnap.child("body").getValue().toString());
+            mList.add(item);
+        }
+
+        return new Guide(id, title, my_race, op_race, author_id, author_email, mList, current_time);
+    }
+
+    /**
+     *
+     * @param tempListGuide
+     */
+    private void handleLoadGuideLogic(List<Guide> tempListGuide) {
+        Collections.reverse(tempListGuide);
+        List<Guide> newList = new ArrayList<>();
+        for (int i = 0; i < tempListGuide.size() - 1; i++) {
+            newList.add(tempListGuide.get(i));
+        }
+        lastItemKey = tempListGuide.get(tempListGuide.size() - 1).getId();
+        // set value to the mutable live data
+        allGuides.setValue(newList);
+    }
+
+
+    /**
+     * @effects:
+     * @param originalList
+     */
+    private void handleLoadMoreLogic (List<Guide> originalList) {
+        Collections.reverse(originalList);
+
+        if (lastItemKey.equals("END")) { // if there are no more to load => return
+            return;
+        }
+
+        if (originalList.size() < guideNumLimit) { // if the number of guides left are less than the minimum per load
+            Objects.requireNonNull(allGuides.getValue()).addAll(originalList);
+            lastItemKey = "END"; // set up key for no more guide
+            return;
+        }
+
+        List<Guide> newList = new ArrayList<>();
+        for (int i = 0; i < originalList.size() - 1; i++) {
+            newList.add(originalList.get(i));
+        } // clone the result from query ( except the last element ) to a new list to add to the current list
+        lastItemKey = originalList.get(originalList.size() - 1).getId(); // the last element => key for load more
+        Objects.requireNonNull(allGuides.getValue()).addAll(newList); // add the newly acquired guides to the list
     }
 }
--- app/src/main/java/com/sc2guide/sc2_guides_android/viewmodel/RaceGuideViewModel.java
+++ app/src/main/java/com/sc2guide/sc2_guides_android/viewmodel/RaceGuideViewModel.java
@@ -11,60 +11,59 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.sc2guide.sc2_guides_android.data.model.Guide;
 import com.sc2guide.sc2_guides_android.data.model.GuideBodyItem;
+import com.sc2guide.sc2_guides_android.error.NotPossibleException;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class RaceGuideViewModel extends ViewModel {
     private MutableLiveData<List<Guide>> raceGuides;
     private String reference = "guides";
+    private int guideNumLimit = 4;
+    private String lastItemKey = "";
+
+    public void setLastItemKey(String lastItemKey) {
+        this.lastItemKey = lastItemKey;
+    }
 
     public LiveData<List<Guide>> getRaceGuides(String race, boolean forceUpdate) {
-        if (raceGuides != null && forceUpdate) {
-            loadAllGuides(race);
+        if (forceUpdate) {
+            loadAllRaceGuides(race);
         }
         if (raceGuides == null) {
             raceGuides = new MutableLiveData<>();
-            loadAllGuides(race);
+            loadAllRaceGuides(race);
         }
         return raceGuides;
     }
 
-    private  void  loadAllGuides(String race) {
+    public LiveData<List<Guide>> getMoreRaceGuides(String race) {
+        loadMoreRaceGuides(race);
+        return raceGuides;
+    }
+
+    private void loadAllRaceGuides(String race) {
         // Do async operation to fetch guides
         FirebaseDatabase.getInstance()
                 .getReference(reference)
                 .orderByChild("myRace")
                 .equalTo(race)
+                .limitToLast(guideNumLimit)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()) {
-                            List<Guide> tempGuidesList = new ArrayList<>();
-                            for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                            List<Guide> tempListGuide = new ArrayList<>();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                                 try {
-                                    String id = snapshot.getKey();
-                                    String title = snapshot.child("title").getValue().toString();
-                                    String my_race = snapshot.child("myRace").getValue().toString();
-                                    String op_race = snapshot.child("opRace").getValue().toString();
-                                    String author_id = snapshot.child("authorId").getValue().toString();
-                                    String author_email = snapshot.child("authorName").getValue().toString();
-                                    String current_time =  snapshot.child("date").getValue().toString();
-                                    List<GuideBodyItem> mList = new ArrayList<>();
-                                    for(DataSnapshot mySnap : snapshot.child("guideBodyItems").getChildren()){
-                                        GuideBodyItem item = new GuideBodyItem(mySnap.child("type").getValue().toString(), mySnap.child("body").getValue().toString());
-                                        mList.add(item);
-                                    }
-
-                                    tempGuidesList.add(new Guide(id, title,my_race,op_race,author_id,author_email, mList, current_time));
-                                } catch (Exception e) {
+                                    tempListGuide.add(retrieveDataToModel(snapshot));
+                                } catch (NotPossibleException e) {
                                     e.printStackTrace();
                                 }
                             }
-
-                            Collections.reverse(tempGuidesList);
-                            raceGuides.setValue(tempGuidesList);
+                            handleLoadGuideLogic(tempListGuide);
                         }
                     }
 
@@ -74,4 +73,101 @@ public class RaceGuideViewModel extends ViewModel {
                     }
                 });
     }
+
+    private void loadMoreRaceGuides(String race) {
+        FirebaseDatabase.getInstance()
+                .getReference(reference)
+                .orderByChild("myRace")
+                .equalTo(race)
+                .limitToLast(guideNumLimit)
+                .endAt(lastItemKey) // TODO: !important : err af, need some other solution
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            List<Guide> tempListGuide = new ArrayList<>();
+                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                try {
+                                    tempListGuide.add(retrieveDataToModel(snapshot));
+                                } catch (NotPossibleException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            handleLoadMoreLogic(tempListGuide);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        databaseError.getCode();
+                    }
+                });
+    }
+
+    /**
+     * @param tempListGuide
+     */
+    private void handleLoadGuideLogic(List<Guide> tempListGuide) {
+        Collections.reverse(tempListGuide);
+        List<Guide> newList = new ArrayList<>();
+        for (int i = 0; i < tempListGuide.size() - 1; i++) {
+            newList.add(tempListGuide.get(i));
+        }
+        lastItemKey = tempListGuide.get(tempListGuide.size() - 1).getId();
+        // set value to the mutable live data
+        raceGuides.setValue(newList);
+    }
+
+    /**
+     * @param originalList
+     * @effects:
+     */
+    private void handleLoadMoreLogic(List<Guide> originalList) {
+        Collections.reverse(originalList);
+
+        if (lastItemKey.equals("END")) { // if there are no more to load => return
+            return;
+        }
+
+        if (originalList.size() < guideNumLimit) { // if the number of guides left are less than the minimum per load
+            Objects.requireNonNull(raceGuides.getValue()).addAll(originalList);
+            lastItemKey = "END"; // set up key for no more guide
+            return;
+        }
+
+        List<Guide> newList = new ArrayList<>();
+        for (int i = 0; i < originalList.size() - 1; i++) {
+            newList.add(originalList.get(i));
+        } // clone the result from query ( except the last element ) to a new list to add to the current list
+        lastItemKey = originalList.get(originalList.size() - 1).getId(); // the last element => key for load more
+        Objects.requireNonNull(raceGuides.getValue()).addAll(newList); // add the newly acquired guides to the list
+    }
+
+    /**
+     * @param snapshot
+     * @return
+     * @throws NotPossibleException
+     * @effects: use the data from the snapshot to make a new Guide();
+     */
+    private Guide retrieveDataToModel(DataSnapshot snapshot) throws NotPossibleException {
+        // get all the attributes
+        String id = snapshot.getKey();
+        String title = snapshot.child("title").getValue().toString();
+        String my_race = snapshot.child("myRace").getValue().toString();
+        String op_race = snapshot.child("opRace").getValue().toString();
+        String author_id = snapshot.child("authorId").getValue().toString();
+        String author_email = snapshot.child("authorName").getValue().toString();
+        String current_time = snapshot.child("date").getValue().toString();
+        // list to hold all the guide body items
+        List<GuideBodyItem> mList = new ArrayList<>();
+        // get guide body items
+        for (DataSnapshot mySnap : snapshot.child("guideBodyItems").getChildren()) {
+            GuideBodyItem item = new GuideBodyItem(mySnap.child("type").getValue().toString(), mySnap.child("body").getValue().toString());
+            mList.add(item);
+        }
+
+        return new Guide(id, title, my_race, op_race, author_id, author_email, mList, current_time);
+    }
+
+
 }
